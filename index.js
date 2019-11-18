c9aff1eee9b7e9c9fa4e5b14df07772acae9b5ed
Ch3my <35230589+Ch3my@users.noreply.github.com>
2019-11-18 11:09:42 -0300

    finish

--- index.js
+++ index.js
@@ -10,132 +10,57 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+
+const mysql = require('mysql')
+var con = mysql.createConnection({
+  host: "192.168.3.2",
+  user: "acceso",
+  password: "acceso",
+  database: "cs313"
+});
+
 app.get('/', (req, res) => res.render('pages/index'))
-app.post('/calculateRate', function (req, res) {
-  var result = {}
-  result.data = {}
-  result.data.weight = req.body.weight
-  result.data.mailType = mailTypeName(req.body.mailType)
-  result.result = calculateRate(req.body.mailType, req.body.weight)
-  res.render('pages/calculateRate', { req, res, result })
+app.get('/getPerson', function (req, res) {
+  getPerson(req.query.id, (err, person) => {
+    res.json(person)
+  })
 })
-app.post('/calculateRateApi', function (req, res) {
-  var result = {}
-  result.data = {}
-  result.data.weight = req.body.weight
-  result.data.mailType = mailTypeName(req.body.mailType)
-  result.result = calculateRate(req.body.mailType, req.body.weight)
-  res.json(result)
+app.get('/getParents', function (req, res) {
+  getParents(req.query.id, (err, person) => {
+    res.json(person)
+  })
+})
+app.get('/getChildren', function (req, res) {
+  getChildren(req.query.id, (err, person) => {
+    res.json(person)
+  })
 })
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
 
-function calculateRate(mailType, weight) {
-  var result = 0
-  switch (mailType) {
-    case 'LS':
-      // Letters Stamped
-      if (weight < 1) {
-        result = 0.55
-      } else if (weight >= 1 && weight < 2) {
-        result = 0.70
-      } else if (weight >= 2 && weight < 3) {
-        result = 0.85
-      } else if (weight >= 3) {
-        result = 1
-      }
-      break
-    case 'LM':
-      // Letters Metered
-      if (weight < 1) {
-        result = 0.50
-      } else if (weight >= 1 && weight < 2) {
-        result = 0.65
-      } else if (weight >= 2 && weight < 3) {
-        result = 0.80
-      } else if (weight >= 3) {
-        result = 0.95
-      }
-      break
-    case 'LE':
-      // large Envelopes
-      if (weight < 1) {
-        result = 1
-      } else if (weight >= 1 && weight < 2) {
-        result = 1.15
-      } else if (weight >= 2 && weight < 3) {
-        result = 1.30
-      } else if (weight >= 3 && weight < 4) {
-        result = 1.45
-      } else if (weight >= 4 && weight < 5) {
-        result = 1.60
-      } else if (weight >= 5 && weight < 6) {
-        result = 1.75
-      } else if (weight >= 6 && weight < 7) {
-        result = 1.90
-      } else if (weight >= 7 && weight < 8) {
-        result = 2.05
-      } else if (weight >= 8 && weight < 9) {
-        result = 2.20
-      } else if (weight >= 9 && weight < 10) {
-        result = 2.35
-      } else if (weight >= 10 && weight < 11) {
-        result = 2.50
-      } else if (weight >= 11 && weight < 12) {
-        result = 2.65
-      } else if (weight >= 12 && weight < 13) {
-        result = 2.80
-      }
-      break
-    case 'FC':
-      // Fist Class- Used Zone 1 & 2 for the prices
-      if (weight < 1) {
-        result = 3.66
-      } else if (weight >= 1 && weight < 2) {
-        result = 3.66
-      } else if (weight >= 2 && weight < 3) {
-        result = 3.66
-      } else if (weight >= 3 && weight < 4) {
-        result = 3.66
-      } else if (weight >= 4 && weight < 5) {
-        result = 4.39
-      } else if (weight >= 5 && weight < 6) {
-        result = 4.39
-      } else if (weight >= 6 && weight < 7) {
-        result = 4.39
-      } else if (weight >= 7 && weight < 8) {
-        result = 4.39
-      } else if (weight >= 8 && weight < 9) {
-        result = 5.19
-      } else if (weight >= 9 && weight < 10) {
-        result = 5.19
-      } else if (weight >= 10 && weight < 11) {
-        result = 5.19
-      } else if (weight >= 11 && weight < 12) {
-        result = 5.19
-      } else if (weight >= 12 && weight < 13) {
-        result = 5.71
-      }
-      break
-  }
-  return result
+function getPerson(id, callback) {
+  con.query("select * from person where id  = " + id, function (err, result, fields) {
+    if (err) throw err;
+    callback(null, result);
+  });
 }
-
-function mailTypeName(code) {
-  switch (code) {
-    case 'LS':
-      return 'Letters (Stamped)'
-      break
-    case 'LM':
-      return 'Letters (Metered)'
-      break
-    case 'LE':
-      return 'Large Envelopes (Flats)'
-      break
-    case 'FC':
-      return 'First-Class Package Service -- Retail'
-      break
-  }
+function getParents(id, callback) {
+  con.query("select f.firstName, f.lastName, f.dateOfBirth  from parents_children pc " +
+    "inner join person p on p.id = pc.fk_personId " +
+    "inner join person as f on f.id = pc.fk_parentId " +
+    "where pc.fk_personId = " + id, function (err, result, fields) {
+      if (err) throw err;
+      callback(null, result);
+    });
+}
+function getChildren(id, callback) {
+  con.query("select p.firstName, p.lastName, p.dateOfBirth  from parents_children pc " +
+    "inner join person p on p.id = pc.fk_personId " +
+    "inner join person as f on f.id = pc.fk_parentId " +
+    "where pc.fk_parentId = " + id, function (err, result, fields) {
+      if (err) throw err;
+      callback(null, result);
+    });
 }
